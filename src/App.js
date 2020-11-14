5a4dcd67a54c91ba734027e979e95ba78b899d29
roman <roman.kotchetov@gmail.com>
2020-11-14 10:55:46 -0800

    Added icons

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
-
+import React, { useState } from 'react';
+    
 function App() {
+    // const [state, setstate] = useState(initialState)};
+
     return (
         <div className="App">
             Hello World
